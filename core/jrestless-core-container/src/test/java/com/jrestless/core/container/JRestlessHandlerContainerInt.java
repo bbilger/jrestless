8a26e711c9b866da7e0b2eff595c17c80bed229f
bbilger <bbilger@bbilger.com>
2016-06-19 22:43:00 +0200

    add license header

--- core/jrestless-core-container/src/test/java/com/jrestless/core/container/JRestlessHandlerContainerInt.java
+++ core/jrestless-core-container/src/test/java/com/jrestless/core/container/JRestlessHandlerContainerInt.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Bjoern Bilger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jrestless.core.container;
 
 import static com.jrestless.test.MockitoExt.emptyBaos;
@@ -44,7 +59,6 @@ import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.jrestless.core.container.JRestlessHandlerContainer;
 import com.jrestless.core.container.dpi.InstanceBinder;
 import com.jrestless.core.container.io.JRestlessRequestContext;
 import com.jrestless.core.container.io.JRestlessResponseWriter;
--- core/jrestless-core-container/src/test/java/com/jrestless/core/container/JRestlessHandlerContainerTest.java
+++ core/jrestless-core-container/src/test/java/com/jrestless/core/container/JRestlessHandlerContainerTest.java
@@ -1,8 +1,31 @@
+/*
+ * Copyright 2016 Bjoern Bilger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jrestless.core.container;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -23,7 +46,6 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import com.jrestless.core.container.JRestlessHandlerContainer;
 import com.jrestless.core.container.io.JRestlessRequestContext;
 
 @RunWith(PowerMockRunner.class)
